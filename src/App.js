bb41a96b6a0fab5ac306c579b35b5907bf941773
张欧文 <1534661514@qq.com>
2023-02-21 21:50:11 +0800

    handle error

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import "./App.css";
 function App() {
   const [stuData, setStuData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   useEffect(() => {
     // 设置loading为正在加载true
     setLoading(true);
-    fetch("http://localhost:1337/api/students")
+    fetch("http://localhost:1337/api/student")
       .then((res) => {
-        return res.json();
+        // 判断响应是否正常返回
+        if (res.ok) {
+          return res.json();
+        }
+
+        // 代码运行到此处,说明没有成功加载到数据
+        setLoading(false);
+
+        throw new Error("数据加载失败!!");
       })
       .then((data) => {
         // 将加载到的数据设置到state中
@@ -18,12 +27,19 @@ function App() {
         // 数据加载完毕
         setLoading(false);
       })
-      .catch(() => {});
+      .catch((e) => {
+        // catch 一执行,说明上述代码执行出错
+        setLoading(false);
+
+        // 设置一个错误状态
+        setError(e.message);
+      });
   }, []);
   return (
     <div className="app">
-      {!loading && <StudentList stus={stuData} />}
+      {!loading && !error && <StudentList stus={stuData} />}
       {loading ? "数据正在加载中" : ""}
+      {error && <p>error</p>}
     </div>
   );
 }
