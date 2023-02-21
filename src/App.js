23102bd149220a9d1fd876ea05c988aa0bd43c27
张欧文 <1534661514@qq.com>
2023-02-21 21:13:03 +0800

    handle loading data

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import StudentList from "./components/StudentList";
 import "./App.css";
 function App() {
   const [stuData, setStuData] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    // 设置loading为正在加载true
+    setLoading(true);
     fetch("http://localhost:1337/api/students")
       .then((res) => {
         return res.json();
@@ -12,12 +14,16 @@ function App() {
       .then((data) => {
         // 将加载到的数据设置到state中
         setStuData(data.data);
+
+        // 数据加载完毕
+        setLoading(false);
       })
       .catch(() => {});
-  });
+  }, []);
   return (
     <div className="app">
-      <StudentList stus={stuData} />
+      {!loading && <StudentList stus={stuData} />}
+      {loading ? "数据正在加载中" : ""}
     </div>
   );
 }
